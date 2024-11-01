80bd7e53686d341ff5356ceef18af480809e3bf3
Simanovich <ivanov@gmail.com>
2024-11-01 12:50:54 +0300

    созданы две функции сложения и вычитания
    добавлена функий\ умнжения и исправлены ошибки

--- test/progaTest.py
+++ test/progaTest.py
@@ -2,6 +2,11 @@ def summa(a,b):
     return a + b
 
 def minus(a,b):
-    return (minus(5,2))
+    return (a-b)
+
+def umnogenie(a, b):
+    return (a*b)
 
 print(summa(2, 3))
+print(minus(5,2))
+print(umnogenie(5,2))
